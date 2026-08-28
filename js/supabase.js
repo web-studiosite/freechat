d076d0787c6a179b7986d77fef962c0490a8fcd8
web-studiosite <felisbertouele7@gmail.com>
2026-08-28 22:14:11 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-export const SUPABASE_URL = "COLE_AQUI_A_URL_DO_SEU_PROJETO";
-export const SUPABASE_ANON_KEY = "COLE_AQUI_A_CHAVE_PUBLICA_ANON";
+export const SUPABASE_URL = "https://lseiteywfkfekohmpxeb.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxzZWl0ZXl3ZmtmZWtvaG1weGViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5NDAyNjcsImV4cCI6MjEwMzUxNjI2N30.BgZCgrZf0lW1XAMkupo9WWE2_Yvn4umpSALKm_Wvyo4";
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
   auth: { persistSession: true, autoRefreshToken: true, detectSessionInUrl: true }
